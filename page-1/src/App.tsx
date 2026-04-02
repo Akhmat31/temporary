55c7d9bb50fdaf74ed3307cf8344e3c3af1d0c3c
encoding <encoding@panteri.GAYO>
2026-04-02 22:10:42 +0700

    layout sementara

---
+++ page-1/src/App.tsx
@@ -0,0 +1,12 @@
+import { Cpages } from './components/section_components/Cpages';
+import { Hero } from './components/section_components/Hero';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Cpages />
+    </div>
+  );
+}
+export default App;
