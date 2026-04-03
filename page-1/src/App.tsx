e1dd0b7e2e6497814e1b9c0a868b05df9f967832
Akhmat31 <encoding@panteri.GAYO>
2026-04-03 16:31:28 +0700

    update: final

--- page-1/src/App.tsx
+++ page-1/src/App.tsx
@@ -1,11 +1,25 @@
+import { Article } from './components/section_components/Article';
+import { Consult } from './components/section_components/Consult';
 import { Cpages } from './components/section_components/Cpages';
+import { Endfooter } from './components/section_components/Endfooter';
 import { Hero } from './components/section_components/Hero';
+import { Proccess } from './components/section_components/Proccess';
+import { Product } from './components/section_components/Product';
+import { PUIThumbs } from './components/section_components/PUIThumbs';
+import { Testimonials } from './components/section_components/Testimonials';
 
 function App() {
   return (
     <div>
       <Hero />
       <Cpages />
+      <PUIThumbs />
+      <Product />
+      <Consult />
+      <Proccess />
+      <Testimonials />
+      <Article />
+      <Endfooter />
     </div>
   );
 }
--- page-1/src/main.tsx
+++ page-1/src/main.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import "./components/section_components/style/product.css";
+import "./components/section_components/style/section-cards.css";
+import "./components/section_components/style/init.css";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
