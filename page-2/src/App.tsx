a64f8532f789d3ee28a0cf546297a173a3147277
Akhmat31 <encoding@panteri.GAYO>
2026-04-04 00:08:16 +0700

    updates_

--- page-2/src/App.tsx
+++ page-2/src/App.tsx
@@ -1,11 +1,19 @@
 import { DuoCard } from './components/section_components/DuoCard';
+import { Endfooter } from './components/section_components/Endfooter';
+import { FinalContext } from './components/section_components/FinalContext';
 import { Hero } from './components/section_components/Hero';
+import { Items } from './components/section_components/Items';
+import { Lab } from './components/section_components/Lab';
 
 function App() {
   return (
     <div>
       <Hero />
       <DuoCard />
+      <Items />
+      <Lab />
+      <FinalContext />
+      <Endfooter />
     </div>
   );
 }
